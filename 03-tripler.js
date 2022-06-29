2502a91e5d26e5f24b5142cdf88988167390882e
etkndr <103692901+etkndr@users.noreply.github.com>
2022-06-29 00:33:30 -0400

    prob 3

--- 03-tripler.js
+++ 03-tripler.js
@@ -1,3 +1,10 @@
+// new arr []
+// loop start 0
+// each loop, mult arr[i] by 3
+// push arr[i] to newArr
+// loop end arr length
+
+
 /*******************************************************************************
 Write a function tripler(array) that takes in an array and returns a new array
 containing 3 times every element of the original array.
@@ -10,6 +17,12 @@ tripler([4, 1, 7]); // => [ 12, 3, 21 ]
 
 function tripler(array) {
   // your code here...
+  let newArr = []
+
+  for (i = 0; i < array.length; i++) {
+    newArr.push(array[i]*3)
+  }
+  return newArr
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
