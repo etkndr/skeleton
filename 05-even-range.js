eaae7ae0c1689a7d8036451eb8b6d01ef598f39b
etkndr <103692901+etkndr@users.noreply.github.com>
2022-06-29 00:55:49 -0400

    prob 5

--- 05-even-range.js
+++ 05-even-range.js
@@ -11,6 +11,16 @@ evenRange(8, 5) => []
 
 function evenRange(start, end) {
   // your code here...
+  let nums = []
+    if (start % 2 === 1) {
+    start += 1
+  }
+
+  for (i = start; i <= end; i += 2) {
+ nums.push(i)
+
+  }
+  return nums
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
